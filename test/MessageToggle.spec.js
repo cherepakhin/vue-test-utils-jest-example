6f927685fcdaabb75f0522d963416c8d4c86725c
Черепахин Василий <vasi.che@gmail.com>
2023-09-16 16:30:07 +0500

    rename MessageToggle1 to MessageToggle

--- test/MessageToggle.spec.js
+++ test/MessageToggle.spec.js
@@ -1,15 +1,19 @@
 import { shallowMount } from '@vue/test-utils'
-import MessageToggle1 from '@/components/MessageToggle1.vue'
+import MessageToggle from '@/components/MessageToggle.vue'
 import Message from '@/components/Message.vue'
 
 describe('MessageToggle.vue', () => {
   it('toggles msg passed to Message when button is clicked', () => {
-    const wrapper = shallowMount(MessageToggle1)
+    const wrapper = shallowMount(MessageToggle)
     const button = wrapper.find('#btn-message')
+
     button.trigger('click')
+
     const MessageComponent = wrapper.find(Message)
     expect(MessageComponent.props()).toEqual({msg: 'message'})
+
     button.trigger('click')
+
     expect(MessageComponent.props()).toEqual({msg: 'toggled message'})
   })
 })
