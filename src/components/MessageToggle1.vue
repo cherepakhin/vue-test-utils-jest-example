9d309f67f7063bbe08acbf0d798433832a4a544c
Черепахин Василий <vasi.che@gmail.com>
2023-09-16 11:30:34 +0500

    Изменил "boot message" пр и загрузке Message.vue

---
+++ src/components/MessageToggle1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <message :msg="msg"></message>
+    <button id="btn-message" @click="toggleMessage">
+      name btn 'Change message"
+    </button>
+  </div>
+</template>
+
+<script>
+import Message from '@/components/Message.vue'
+
+export default {
+  // почему-то 'name' никак не влияет на использование в App.vue.
+  // Конвертирует по имени файла??? ДААА! MessageToggle1 -> <message-toggle1/>, 
+  // если явно не указано "name"
+  // Переименовал файл в MessageToggle1 и в App теперь используется <message-toggle1/> !!!
+  // Закомментил name и все-равно работает.!!!
+  // name: 'message-toggle2', // это исходная строка, 
+  // ее закоментил и компонент MessageToggle1.vue доступен <message-toggle1/>
+  data: () => ({
+    msg: null
+  }),
+  methods: {
+    toggleMessage () {
+      this.msg = this.msg === 'message' ? 'toggled message' : 'message'
+    }
+  },
+  components: {
+    Message
+  }
+}
+</script>
